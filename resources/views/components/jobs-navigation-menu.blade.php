bf4cc4e3cea674e8317ea1df42550886f9bb55cf
Goldoni <fotsa.goldoni@yahoo.fr>
2022-03-22 05:28:55 +0100

    navigation

--- resources/views/components/jobs-navigation-menu.blade.php
+++ resources/views/components/jobs-navigation-menu.blade.php
@@ -1,18 +1,20 @@
 <header class="bg-white shadow" x-data="{ open: false }" @keydown.window.escape="open = false">
     <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
-        <div class="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
-            <div class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
-                <div class="flex-shrink-0 flex items-center">
+        <div class="relative flex items-center justify-between h-16">
+            <!-- Logo section -->
+            <div class="flex items-center px-2 lg:px-0 xl:w-64">
+                <div class="flex-shrink-0">
                     <a href="{{ url('/home') }}">
-                        <img class="block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow">
+                        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow">
                     </a>
                 </div>
             </div>
-            <div class="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+
+            <!-- Search section -->
+            <div class="flex-1 flex justify-center lg:justify-end">
                 @livewire('jobs.search-bar')
             </div>
-
-            <div class="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden">
+            <div class="flex lg:hidden">
                 <!-- Mobile menu button -->
                 <button
                     @click="open = ! open"
@@ -24,12 +26,12 @@
                     <!--
                       Icon when menu is closed.
 
-                      Heroicon name: outline/menu
+                      Heroicon name: outline/menu-alt-1
 
                       Menu open: "hidden", Menu closed: "block"
                     -->
-                    <svg :class="{ 'hidden': open, 'block': !(open) }" class="h-6 w-6 block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    <svg :class="{ 'hidden': open, 'block': !(open) }" class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
                     <!--
                       Icon when menu is open.
@@ -38,12 +40,12 @@
 
                       Menu open: "block", Menu closed: "hidden"
                     -->
-                    <svg :class="{ 'block': open, 'hidden': !(open) }" class="h-6 w-6 hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg :class="{ 'block': open, 'hidden': !(open) }" class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
-
+            <!-- Links section -->
             @auth
                 <div class="hidden lg:flex lg:items-center lg:justify-end xl:col-span-4">
                     <a href="#" class="ml-5 flex-shrink-0 bg-white rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
--- resources/views/livewire/jobs/search-bar.blade.php
+++ resources/views/livewire/jobs/search-bar.blade.php
@@ -1,6 +1,4 @@
-<div class="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
-    <div class="w-full">
-        <label for="search" class="sr-only">Search</label>
-        <x-input.jobs-search :items="$jobs" wire:model="searchJob" wire:key="search-jobs"></x-input.jobs-search>
-    </div>
+<div class="w-full px-2 lg:px-6">
+    <label for="search" class="sr-only">Search projects</label>
+    <x-input.jobs-search :items="$jobs" wire:model="searchJob" wire:key="search-jobs"></x-input.jobs-search>
 </div>
