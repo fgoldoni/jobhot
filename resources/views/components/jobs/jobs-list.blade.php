06816086a24b5c8f9bce252852173ff12cb961e2
Goldoni <fotsa.goldoni@yahoo.fr>
2022-03-23 06:08:40 +0100

    register

--- resources/views/components/jobs/jobs-list.blade.php
+++ resources/views/components/jobs/jobs-list.blade.php
@@ -1,6 +1,7 @@
 @props(['rows'])
 
 <div class="bg-white shadow overflow-hidden sm:rounded-md">
+    <div class="pl-10 py-4">About {{ $rows->total() }} results ...</div>
     <ul role="list" class="divide-y space-y-5 p-4 divide-gray-200">
         @forelse($rows as $row)
             <x-jobs.item :row="$row" wire:key="item-{{$row->id}}"></x-jobs.item>
