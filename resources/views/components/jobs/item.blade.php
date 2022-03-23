57f11a233bc8b2ce080858e812cd3e873d762fff
Goldoni <fotsa.goldoni@yahoo.fr>
2022-03-23 05:05:56 +0100

    register

--- resources/views/components/jobs/item.blade.php
+++ resources/views/components/jobs/item.blade.php
@@ -62,13 +62,13 @@
 
             <div class="flex space-x-6">
                 <span class="inline-flex items-center text-sm">
-                  <button type="button" class="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+                  <button type="button" class="inline-flex items-center space-x-2 text-gray-400 hover:text-gray-500">
                     <!-- Heroicon name: solid/eye -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
                       </svg>
-                    <span class="font-medium text-gray-900">$120k – $140k</span>
+                    <span class="font-medium text-gray-900 text-xs md:text-sm">$120k – $140k</span>
                     <span class="sr-only">Salary expectation</span>
                   </button>
                 </span>
@@ -79,11 +79,11 @@
                     <span class="inline-flex items-center text-sm">
                       <button type="button" class="inline-flex items-center space-x-2 text-gray-400 hover:text-gray-500">
 
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                               <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
                           </svg>
 
-                          <span class="font-medium text-gray-900">
+                          <span class="font-medium text-gray-900 text-xs md:text-sm">
                              {{ $row->city_id ? $row->city->name : $row->division->name }}, {{ $row->country->name }}
                           </span>
 
