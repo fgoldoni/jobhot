57dd72704a34f9708bb7f01bb2a25ec94e47a9e0
Goldoni <fotsa.goldoni@yahoo.fr>
2022-03-29 02:10:19 +0200

    jobs

---
+++ app/Enums/SalaryType.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Enums;
+
+/**
+ * Class SalaryType
+ *
+ * @package \App\Enums
+ */
+enum SalaryType: string
+{
+    case Hour = 'hour';
+    case Day = 'day';
+    case Month = 'month';
+    case Year = 'year';
+}
---
+++ app/Http/Livewire/Admin/Datatable/WithSalaryTypes.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Datatable;
+
+use App\Enums\JobState;
+use App\Enums\SalaryType;
+use Illuminate\Support\Collection as StatesCollection;
+
+trait WithSalaryTypes
+{
+    public ?int $selectedSalaryType;
+
+    public StatesCollection $salaryTypes;
+
+
+    public function mountWithSalaryTypes()
+    {
+        $this->salaryTypes = $this->getSalaryTypes();
+        $this->selectedSalaryType = $this->findSalaryTypeBy('name', ucfirst($this->editing->salary_type->value))['id'];
+    }
+
+    private function findSalaryTypeBy(string $key, $value): array
+    {
+        return $this->salaryTypes->filter(fn ($s) => $s[$key] === $value)->first();
+    }
+
+    public function updatedSelectedSalaryType ($value)
+    {
+        $this->editing->salary_type = strtolower($this->findSalaryTypeBy('id', $this->selectedSalaryType)['name']);
+    }
+
+    private function getSalaryTypes(): StatesCollection
+    {
+        return collect([
+            [
+                'id' => 0,
+                'name' => ucfirst((SalaryType::Hour)->value),
+            ],
+            [
+                'id' => 1,
+                'name' => ucfirst((SalaryType::Day)->value),
+            ],
+            [
+                'id' => 2,
+                'name' => ucfirst((SalaryType::Month)->value),
+            ],
+            [
+                'id' => 3,
+                'name' => ucfirst((SalaryType::Year)->value),
+            ]
+        ]);
+    }
+}
